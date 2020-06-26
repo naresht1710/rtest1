9ddb3b2078e8c5d579535430b64d81c510f11f06
naresht1710 <naresht1710@gmail.com>
2020-06-26 15:48:02 +0530

    test test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1 data-testid="test">Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start e diting to see some magic happen!</h2>
     </div>
   );
 }
